091f273ba3e30137f6313b6bb520170b3cbc3f34
Glitch (stark-code-bos-altyapi) <none>
2019-11-22 11:27:09 +0000

    🏨🔦 Checkpoint ./komutlar/yardım.js:5472946/935

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -4,11 +4,20 @@ exports.run = async(client, message, args) => {
  
  const Embed = new Discord.RichEmbed()
  .setAuthor("MC-AT", client.user.avatarURL)
- .setTitle(`[MC-AT BOT](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8)`)
+
  .setDescription(`
+[MC-AT BOT](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8)
 **!yardım**, ile yardım alabilirsiniz.
 Örnek komut kullanımı: \`!küfüraç\`
 Botu davet etmek için: \`!davet\`
+`)
+ .addField("!komutlar (13)", `
+herkesin kullanabileceği standart komutlar;
+\`botbilgi\`,\`davet\`,\`oyver\`,\`gold\`,\`webpanel\`,\`sunucu-pp\`,\`profil\`,\`kredim\`,\`p-market\`,\`p-menü\`,\`puanım\`,\`ayarlar\`
+`)
+ .addField("!eğlence (9)", `
+herkesin kullanabileceği eğlence komutları;
+\`kralol\`,\`maymunol\`,\`yılanol\`,\`adamol\`,\`kediol\`,\`aşk\`,\`token\`,\`tokat\`
 `)
  .setFooter("© Mcadventuretime.com", client.user.avatarURL)
  message.channel.send(Embed)
