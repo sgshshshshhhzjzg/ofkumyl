6c67b59f31d73abb0003c286374adbe0ed06bdf9
Glitch (flat-caraway) <none>
2019-11-24 12:14:23 +0000

    🐘😬 Checkpoint ./komutlar/yavaşmod.js:5472946/4696

--- komutlar/yavaşmod.js
+++ komutlar/yavaşmod.js
@@ -1,47 +1,60 @@
 const Discord = require('discord.js'); 
  
-exports.run = async(client, message, args) => {
+exports.run = async(client, msg, args) => {
  
- const Embed = new Discord.RichEmbed()
- .setAuthor("EŞŞEK-AT", client.user.avatarURL)
-.setColor("BLUE")
- .setDescription(`
-[MC-AT BOT](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8)
-**!yardım**, ile yardım alabilirsiniz.
-Örnek komut kullanımı: \`!küfüraç\`
-Botu davet etmek için: \`!davet\`
-`)
- .addField("!komutlar (13)", `
-herkesin kullanabileceği standart komutlar;
-\`botbilgi\`,\`davet\`,\`oyver\`,\`gold\`,\`webpanel\`,\`sunucu-pp\`,\`profil\`,\`kredim\`,\`p-market\`,\`p-menü\`,\`puanım\`,\`ayarlar\`
-`)
- .addField("!eğlence (9)", `
-herkesin kullanabileceği eğlence komutları;
-\`kralol\`,\`maymunol\`,\`yılanol\`,\`adamol\`,\`kediol\`,\`aşk\`,\`token\`,\`tokat\`
-`)
- 
- .addField("!moderasyon (13)", `
-yetkililer için moderasyon komutları;
-\`küfür\`,\`reklam\`,\`link engel\`,\`reklam kick\`,\`reklam isim ban\`,\`oto rol\`,\`sayaç\`,\`ban koruma sistemi\`,\`duyuru\`,\`oto cevap\`,\`mute sistemi\`,\`anti raid bot sistemi\`
-`)
- 
- .addField("!moderasyon 2 (12)", `
-yetkililer için moderasyon komutlarının 2. bölümü;
-\`sil\`,\`reklam taraması\`,\`resimli hg bb\`,\`sunucu tanıt\`,\`oto bot silici\`,\`ultra sohbet temizleyici\`,\`yavaş mod\`,\`duyuru\`,\`tag sistemi\`,\`rol sistemi\`,\`geçici oda sistemi\`,\`kayıt sistemi\`
-`)
- 
- .addField("!koruma (5)", `
-Sunucunuz İçin Koruma Sistemleri.
-\`sağ tık ban koruması\`,\`kanal silme koruması\`,\`rol silme koruması\`,\`sunucuya bot sokma koruması\`,\`güvenlik odası\`
-`)
- .setFooter("© Mcadventuretime.com", client.user.avatarURL)
- message.channel.send(Embed)
+ if (msg.channel.type !== "text") return;
+  
+const limit = args[0];
+  
+  if(!limit) {
+              var embed = new Discord.RichEmbed()
+                .setDescription("Doğru kullanım: `!yavaş-mod [0-∞]`")
+              .setColor("RANDOM")
+            msg.channel.send({embed: embed})
+            return
+          }
+  
+if (isNaN(limit)) {
+  var s = new Discord.RichEmbed()
+  .setDescription("Doğru kullanım: `!yavaş-mod [0-∞]`")
+  .setColor("RANDOM")
+  msg.channel.send({embed: s});
+    return
+}
+  
+if (limit > 300) {
+  var x = new Discord.RichEmbed()
+  if (db.has(`premium_${msg.guild.id}`) == false) {
+  return   msg.reply(`5 Dakika Üstü İçin Sunucunuzun Premium Olması Gerekiyor (6 Saate Kadar Uzatabilirsiniz.).
+Maalesef Premium Süresi Bitmiştir. Hizmetimizden Memnun Olduysanız Tekrar Alabilirsiniz \`!premium\`
+  } else {
+
+  }
+`);
+    return
+}
+    var e = new Discord.RichEmbed()
+    .setDescription(`Yazma süre limiti **${limit}** Saniye olarak ayarlanmıştır!`)
+    .setColor("RANDOM")
+    msg.channel.send({embed: e})
+  
+var request = require('request');
+request({
+    url: `https://discordapp.com/api/v7/channels/${msg.channel.id}`,
+    method: "PATCH",
+    json: {
+        rate_limit_per_user: limit
+    },
+    headers: {
+        "Authorization": `Bot ${client.token}`
+    },
+})
 }
 
 module.exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["31"],
+  aliases: ["yavaşmod"],
   permLevel: 0
 };
 
