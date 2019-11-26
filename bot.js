479e78048e466f14a865bd56be74d09268781799
Glitch (flat-caraway) <none>
2019-11-26 16:30:26 +0000

    🐧🏀 Checkpoint ./komutlar/otorol-ayarla.js:5472946/128 ./bot.js:5472946/34 ./komutlar/otorol-kapat.js:5472946/2184

--- bot.js
+++ bot.js
@@ -140,14 +140,13 @@ client.on("guildMemberAdd", async member => {
 
   if (!mesaj) {
     client.channels.get(kanal).send(":loudspeaker: :inbox_tray: Otomatik Rol Verildi Seninle Beraber `" + member.guild.memberCount + "` Kişiyiz! Hoşgeldin! `" + member.user.username + "`");
-    member.addRole(rol);
-    return;
+    return member.addRole(rol);
   }
 
   if (mesaj) {
     var mesajs = mesaj.replace("-uye-", `${member.user.tag}`).replace("-rol-", `${member.guild.roles.get(rol).name}`).replace("-server-", `${member.guild.name}`).replace("-uyesayisi-", `${member.guild.memberCount}`).replace("-botsayisi-", `${member.guild.members.filter(m => m.user.bot).size}`).replace("-bolge-", `${member.guild.region}`).replace("-kanalsayisi-", `${member.guild.channels.size}`);
     member.addRole(rol);
-    client.channels.get(kanal).send(mesajs);
+    return client.channels.get(kanal).send(mesajs);
      }
 });
 
--- komutlar/otorol-kapat.js
+++ komutlar/otorol-kapat.js
@@ -1,9 +1,7 @@
 const Discord = require('discord.js');
 const db = require('quick.db')
 exports.run = (client, message, args) => { 
-  
-let rol = message.mentions.roles.first() 
-let kanal = message.mentions.channels.first()
+
 if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
  
  if(!rol) return message.channel.send(`
@@ -20,25 +18,22 @@ ${client.emojis.get("647760202875142154")} Ayarlamam İçin Bir Kanal Etiketleme
 Eğer Herşey Zorsa Siteden Kolayca Ayarla : https://www.mcadventuretime.com/dc/
 `)
  
-  message.channel.send(`╔▬▬▬▬▬▬▬▬Otorol▬▬▬▬▬▬▬▬▬
-║► ${client.emojis.get("647746144155467786")} Otorol Aktif Edildi.
-║► ${client.emojis.get("647746144155467786")} **${rol}** Olarak Güncelledim! 
-║► ${client.emojis.get("647746144155467786")} Kayıt Kanalını **${kanal}** Olarak Güncelledim! 
-╚▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬`)
+  message.channel.send(`Bu özellik başarıyla kapatıldı. :hayirr:`)
 
  
-  db.set(`otoRL_${message.guild.id}`, rol.id)  
-  db.set(`otoRK_${message.guild.id}`, kanal.id) 
+  db.delete(`otoRL_${message.guild.id}`)  
+  db.delete(`otoRK_${message.guild.id}`) 
+  db.delete(`otoRM_${message.guild.id}`)  
 };
 exports.conf = {
   enabled: true,  
   guildOnly: false, 
-  aliases: [], 
+  aliases: ["otorol-kapat", "otorolkapat"], 
   permLevel: 0
 };
 
 exports.help = {
-  name: 'oto-rol-ayarla',
+  name: 'oto-rol-kapat',
   description: 'taslak', 
   usage: 'Otorol-ayarla'
 };
