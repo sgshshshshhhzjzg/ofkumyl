c0cd72fd059f0ecc8ed60d227a6f12d25aa1ce5e
Glitch (flat-caraway) <none>
2019-11-27 17:47:46 +0000

    🌅🐬 Checkpoint ./komutlar/reklamkapat.js:5472946/7 ./komutlar/linkaç.js:5472946/767 ./komutlar/linkkapat.js:5472946/736 ./bot.js:5472946/56662 ./komutlar/küfüraç.js:5472946/841 ./komutlar/küfürkapat.js:5472946/743

--- komutlar/küfüraç.js
+++ komutlar/küfüraç.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 const db = require('quick.db')
 exports.run = (client, message, args) => { 
       if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(` Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
-  if (!db.fetch(`reklamK_${message.channel.id}`)) {
-  return message.reply(`Sanırım bu özellik zaten kapalıymış :slight_smile:`)
+  if (db.fetch(`küfürE_${message.channel.id}`)) {
+  return message.reply(`Sanırım bu özellik zaten açıkmış :slight_smile:`)
 }
-  db.delete(`reklamK_${message.channel.id}`)
-  message.reply(`Bu özellik **sadece bu kanalda** devre dışı bırakıldı.`)
+  db.set(`küfürE_${message.channel.id}`, "aktif")
+  message.reply(`Bu özellik **başarıyla açıldı.** ${client.emojis.get("647746144155467786")}`)
 };
 exports.conf = {
   enabled: true,  
@@ -16,7 +16,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'reklamkapat',
+  name: 'küfüraç',
   description: 'sayaç', 
   usage: 'sayaç'
 };
--- komutlar/küfürkapat.js
+++ komutlar/küfürkapat.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js');
 const db = require('quick.db')
 exports.run = (client, message, args) => { 
       if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(` Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
-  if (!db.fetch(`reklamK_${message.channel.id}`)) {
+  if (!db.fetch(`küfürE_${message.channel.id}`)) {
   return message.reply(`Sanırım bu özellik zaten kapalıymış :slight_smile:`)
 }
-  db.delete(`reklamK_${message.channel.id}`)
+  db.delete(`küfürE_${message.channel.id}`)
   message.reply(`Bu özellik **sadece bu kanalda** devre dışı bırakıldı.`)
 };
 exports.conf = {
@@ -16,7 +16,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'reklamkapat',
+  name: 'küfürkapat',
   description: 'sayaç', 
   usage: 'sayaç'
 };
