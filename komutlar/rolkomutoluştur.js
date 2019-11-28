13c9930ffe53f814b48417ce5842e2ab7bfda95a
Glitch (flat-caraway) <none>
2019-11-28 12:37:47 +0000

    🐘🕷 Checkpoint ./komutlar/rolkomutoluştur.js:5472946/952 ./komut.json:5472946/6

--- komutlar/rolkomutoluştur.js
+++ komutlar/rolkomutoluştur.js
@@ -1,12 +1,26 @@
 const Discord = require('discord.js');
 const db = require('quick.db')
-
+const fs = require('fs')
 exports.run = async (client, message,args) => {
 const komut = args[0]  
 
 if (komut.length <= 1) {
   return message.channel.send(`Komutunuz En Az \`1\` Haneli Olmalıdır.`)
 }
+  let komutvarsa = JSON.parse(fs.readFileSync("./komut.json", "utf-8"))
+  let komutvarmi = komutvarsa[message.guild.id].komutlar
+if (komutvarmi.includes(komutvarmi)) {
+  return message.channel.send(`
+Aynı Komutu Daha Önce Kullandığınızı Görüyorum Lütfen Onu Yapılandırın
+Örnek Ayar \`!rol-düzenle ${komut}\``)
+}
+      let komutt = JSON.parse(fs.readFileSync("./komut.json", "utf8"));
+        komutt[message.guild.id] = {
+            komutlar: komut
+        };
+        fs.writeFile("./komut.json", JSON.stringify(komutt), (err) => {
+            if (err) console.log(err);
+        });
      db.set(`${komut}_${message.guild.id}`, "aktif")
      message.channel.send(`
 Rol Sistemi İçin Komutunuz Başarıyla Oluşturuldu Komutunuz **!${komut}**
@@ -16,6 +30,11 @@ Bu Komutu Ayarlamak İçin \`!rol-düzenle ${komut}\` Yazınız
 Tüm Komutları Görmek İçinde \`!rol-komutlar\` Komutunu Giriniz
 Eğer Silmek İsterseniz \`!rol-komut-sil ${komut}\`
 `)
+  
+  /*
+  let autorole =  JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
+  let role = autorole[member.guild.id].sayi
+      */
 }
 
 module.exports.conf = {
