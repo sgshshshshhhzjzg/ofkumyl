730d7552d0052c7b820788e211033202479e1956
Glitch (flat-caraway) <none>
2019-11-29 19:42:22 +0000

    🍉⛪️ Checkpoint ./komutlar/eg-test.js:5472946/1068

--- komutlar/eg-test.js
+++ komutlar/eg-test.js
@@ -6,13 +6,13 @@ exports.run = (client, message, args) => {
 
  var user = message.mentions.users.first() || message.author; 
   
-  Jimp.read(`https://cdn.discordapp.com/attachments/649581228835602432/650052923728068628/unknown.png`, (err, image) => {
-    image.resize(794, 598)
-image.greyscale();                // remove colour from the image
-
-    Jimp.read(user.avatarURL, (err, avatar) => {
-        avatar.resize(275, 275) 
-        image.composite(avatar, 269, 39).write(`./resimler/kralol/kralol-${message.author.id}.png`);
+  Jimp.read(`https://cdn.discordapp.com/attachments/649581228835602432/650052923728068628/unknown.png`, async(err, image) => {
+    await image.resize(794, 598)
+        var font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+        await image.print(font, 272, 349, "3 GÜN");
+    Jimp.read(user.avatarURL, async(err, avatar) => {
+        await avatar.resize(275, 275) 
+      await image.composite(avatar, 269, 39).write(`./resimler/kralol/kralol-${message.author.id}.png`);
         setTimeout(function() {
             message.channel.send(new Discord.Attachment(`./resimler/kralol/kralol-${message.author.id}.png`));
         }, 1000);
