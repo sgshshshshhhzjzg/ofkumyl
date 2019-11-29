eed619c70003ea6bcc85386dee19acafa2441641
Glitch (flat-caraway) <none>
2019-11-29 19:54:28 +0000

    🦄🍗 Checkpoint ./komutlar/eg-test.js:5472946/65 ./komutlar/gold-p-topaz.js:5472946/1781

--- komutlar/gold-p-topaz.js
+++ komutlar/gold-p-topaz.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const Jimp = require('jimp');
+
 const db = require('quick.db')
 const fs = require("fs")
 const ms = require("ms")
@@ -13,11 +15,30 @@ let kod31 = "";
   }
  
  db.add(`goldsure_{message.author.id}`, kod31)
- return message.channel.send(kod31)
+db.delete(`goldsure_{message.author.id}`)
+ var user = message.author; 
+  
+  Jimp.read(`https://cdn.discordapp.com/attachments/649581228835602432/650052923728068628/unknown.png`, async(err, image) => {
+    await image.resize(794, 598)
+        var font = await Jimp.loadFont(Jimp.FONT_SANS_128_WHITE);
+        await image.print(font, 220, 309, kod31 + " GÜN");
+    Jimp.read(user.avatarURL, async(err, avatar) => {
+        await avatar.resize(275, 275) 
+      await image.composite(avatar, 269, 39).write(`./resimler/gold/gold-${message.author.id}.png`);
+        setTimeout(function() {
+            message.reply(`Bravo! **Topaz Kasadan! (${kod31})** Gün Gold Üye Kazandın Azim İle Yapamıyacağın Pek Birşey Yok Sanırım Gold Üyeni Verdim Artık Kullanabilirsin Hayırlı Olsun.`)
+            return message.channel.send(new Discord.Attachment(`./resimler/gold/gold-${message.author.id}.png`));
+        }, 1000);
+      });
+    return
+    });
+
+
     
+  } else {
+  return message.reply("Puanınız Yetersizdir.")  
   }
   
-  return message.reply("Puanınız Yetersizdir.")
 }
 
 module.exports.conf = {
