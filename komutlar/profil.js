a45f24ec59c3847013a495e71b96bb0395b129a8
Glitch (flat-caraway) <none>
2019-11-30 16:30:24 +0000

    📡🍅 Checkpoint ./komutlar/profil.js:5472946/769

--- komutlar/profil.js
+++ komutlar/profil.js
@@ -25,7 +25,10 @@ exports.run = async(client, message, args) => {
 .addField(":bust_in_silhouette: Ad", message.author.tag, true)
 .addField(client.emojis.get("649963065697107978") + " Puan", db.fetch(`goldpuan_${message.author.id}`), true) 
 .addField(":game_die: ID", message.author.id, true)
-
+.addField(":rosette: Durumu", message.author.presence.status.replace("dnd", "Rahatsız Etmeyin").replace("idle", "Boşta").replace("offline", "Çevrimdışı").replace("online", "Çevrimiçi"), true)
+        .addField(':video_game: Oynuyor bölümü', message.author.presence.game ? message.author.presence.game.name : 'Boş.', true)
+.addField(":calendar_spiral: Hesabın oluşturulduğu tarih", message.author.createdAt.toUTCString().replace("Nov", "Kasım").replace("Jan", "Ocak").replace("Feb", "Şubat").replace("Mar", "Mart").replace("Aug", "Ağustos").replace("Sep", "Eylül").replace("Oct", "Ekim").replace("Fri", "Cuma").replace("Mon", "Pazartesi").replace("Sun", "Pazar").replace("Sat", "Cumartesi").replace("Tue", "Salı").replace("Wed", "Çarşamba").replace("Thu", "Perşembe"))
+ 
  
  return message.channel.send(Embed2)
 }
