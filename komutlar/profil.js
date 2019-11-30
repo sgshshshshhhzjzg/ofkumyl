fe1783446dd72a2232d21dd3f0008ad4d716dc59
Glitch (flat-caraway) <none>
2019-11-30 16:06:23 +0000

    🍄🔨 Checkpoint ./komutlar/profil.js:5472946/1348

--- komutlar/profil.js
+++ komutlar/profil.js
@@ -5,14 +5,15 @@ exports.run = async(client, message, args) => {
   if (!db.fetch(`goldpuan_${message.author.id}`)) {
     const Embed = new Discord.RichEmbed()
  
- .setAuthor(message.author.tag, message.author.avatarURL)
+ .setAuthor(message.author.username, message.author.avatarURL)
 .setColor("RED")
 .setThumbnail(message.author.avatarURL)
-.addField(":bust_in_silhouette: Ad", message.author.tag)
- .addField(client.emojis.get("649963065697107978") + " Puan", "0")
- .addField(":game_die: ID", message.author.id)
- .addField(":rosette: Durumu", message.guild.members.filter(m => 
-                                                            m.presence.status()))
+.addField(":bust_in_silhouette: Ad", message.author.tag, true)
+ .addField(client.emojis.get("649963065697107978") + " Puan", "0", true)
+ .addField(":game_die: ID", message.author.id, true)
+ .addField(":rosette: Durumu", message.author.presence.status.replace("dnd", "Rahatsız Etmeyin").replace("idle", "Boşta").replace("offline", "Çevrimdışı").replace("online", "Çevrimiçi"), true)
+        .addField(':video_game: Oynuyor bölümü', message.author.presence.game ? message.author.presence.game.name : 'Boş.', true)
+.addField(":calendar_spiral: Hesabın oluşturulduğu tarih", message.author.createdAt.toUTCString().replace("Nov", "Kasım").replace("Jan", "Ocak").replace("Feb", "Şubat").replace("Mar", "Mart").replace("Aug", "Ağustos").replace("Sep", "Eylül").replace("Oct", "Ekim").replace("Fri", "Cuma").replace("Mon", "Pazartesi").replace("Sun", "Pazar").replace("Sat", "Cumartesi").replace("Tue", "Salı").replace("Wed", "Çarşamba").replace("Thu", "Perşembe"), true)
  
  return message.channel.send(Embed)
   }
