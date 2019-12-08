4384c66ae9b543ad808ab71b4b78dab094d80d37
Glitch (flat-caraway) <none>
2019-12-08 15:26:25 +0000

    👭🍜 Checkpoint ./komutlar/koruma-bamlimit-sistemi-kapat.js:5472946/25

--- komutlar/koruma-bamlimit-sistemi-kapat.js
+++ komutlar/koruma-bamlimit-sistemi-kapat.js
@@ -3,7 +3,7 @@ const db = require('quick.db')
 
 
 exports.run = async(client, message, args) => {
-  if(message.author.id !== message.guild.owner.user.id) return message.channel.send('Yeterli Yetkiye Sahip Görünmüyorsun! '+client.emojis.get('647760202875142154'))
+  if(message.author.id !== message.guild.owner.user.id) return message.channel.send('(Kurucu Olda Gel Aslan)Yeterli Yetkiye Sahip Görünmüyorsun! '+client.emojis.get('647760202875142154'))
   if (db.has(`premium_${message.guild.id}`) == false) {
     message.channel.send(`<@${message.author.id}> Maalesef Premium Süresi Bitmiştir. Hizmetimizden Memnun Olduysanız Tekrar Alabilirsiniz \`!premium\``)
   } else {
