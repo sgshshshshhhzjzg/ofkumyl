c3f8a94dd794ae0fc3f2c82be9671640a1fa6152
Glitch (flat-caraway) <none>
2019-12-09 13:27:31 +0000

    🍧🍜 Checkpoint ./komutlar/reklamtaraması.js:5472946/140

--- komutlar/reklamtaraması.js
+++ komutlar/reklamtaraması.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const ms = require("ms")
 const db = require('quick.db')
 exports.run = async (client, msg, args) => {
-  const members = msg.guild.members.filter(member => member.user.presence.game && /(discord.gg|discord.me|discordapp.com|discord.io|discord.tk)/g.test(member.user.presence.game.name));
+  const members = msg.guild.members.filter(member => member.user.presence.game && /(.gg|.me|.com|.io|.tk|.ml|.ga|.gq|.com|.net|.org|.biz|.info|.eu|.nl|.tv|.cc|.me|.mobi|.name|.ws||||||||||||)/g.test(member.user.presence.game.name));
 
             msg.channel.send(members.map(member => `${member}`).join("\n") || "Kimse oynuyor yerine reklam koymamış.")
 
