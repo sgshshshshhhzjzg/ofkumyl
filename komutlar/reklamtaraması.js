35b3c8b002366002ad1b57640128c1a0d4bc99ce
Glitch (flat-caraway) <none>
2019-12-09 13:51:29 +0000

    🦄🏩 Checkpoint ./komutlar/reklamtaraması.js:5472946/1152

--- komutlar/reklamtaraması.js
+++ komutlar/reklamtaraması.js
@@ -4,11 +4,13 @@ const ms = require("ms")
 const db = require('quick.db')
 exports.run = async (client, msg, args) => {
 
-  const members = msg.guild.members.filter(member => /(.cf|.gg|.me|.com|.io|.tk|.ml|.ga|.gq|.com|.net|.org|.biz|.info|.eu|.nl|.tv|.cc|.me|.mobi|.name|.ws|.academy|.accountant|.accountants|.xyz|.actor|.agency|.apartments|.apartments|.bilgi|.garden|.xyz|.gov)/g.test(member.user.username));
+  const members = msg.guild.members.filter(member => /(.cf|.gg|.me|.com|.io|.tk|.ml|.ga|.gq|.com|.net|.org|.biz|.info|.eu|.nl|.tv|.cc|.me|.mobi|.name|.ws|.academy|.accountant|.accountants|.xyz|.actor|.agency|.apartments|.apartments|.bilgi|.garden|.xyz|.gov)/g.test(member.user.username))
 
             msg.channel.send(members.map(member => {
-              if ()
-              `${member}`
+              if (!member.user.username.includes("|")) {
+              return `${member}`                
+              }
+
             }).join("\n") || "Kimse oynuyor yerine reklam koymamış.")
 
 };
