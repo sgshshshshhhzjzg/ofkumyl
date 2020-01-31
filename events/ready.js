525ac634739298726c90577a31abd879aaa8ae6c
Glitch (flat-caraway) <none>
2020-01-31 11:02:31 +0000

    🍓✈️ Checkpoint ./events/ready.js:6978201/1054

--- events/ready.js
+++ events/ready.js
@@ -8,13 +8,14 @@ var prefix = ayarlar.prefix;
 
 module.exports = client => {
   console.log(`${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("dnd");
+  client.user.setStatus("online");
   //idle = boşta
   //dnd = rahatsız etmeyin
   //online = çevrimiçi
   console.log(`${client.user.id}                                                                                                                                                                     `)
-  client.user.setActivity(`${prefix}yardım | ${client.guilds.size} sunucu | ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Kullanıcıyı`, { type: "LISTENING"});
-  //LISTENING = DİNLİYOR
+  //client.user.setActivity(`${prefix}yardım | ${client.guilds.size} sunucu | ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Kullanıcıyı`, { type: "LISTENING"});
+client.user.setActivity(`!YENİYIL🔥 !premium🔥 !steam🔥 !webpanel🔥 !yardım🔥 + !gold🔥 + BETA 0.8.4`, { type: "WATCHING"});  
+//LISTENING = DİNLİYOR
   //WATCHING = İZLİYOR
   //PLAYING = OYNUYOR 
   console.log(`${client.user.username}: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
